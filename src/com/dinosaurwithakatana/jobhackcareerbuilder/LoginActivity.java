ad12145884a6bd46fbf9c471561de1771b9501c7
Vishnu Rajeevan <Vishnu Rajeevan>
2012-10-13 12:05:43 -0400

    actually resources

---
+++ src/com/dinosaurwithakatana/jobhackcareerbuilder/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.dinosaurwithakatana.jobhackcareerbuilder;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class LoginActivity extends SherlockActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
