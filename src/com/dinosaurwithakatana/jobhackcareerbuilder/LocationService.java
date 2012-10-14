6d5ddba0cc4fd5025988da75e17d0801b8118c5e
Vishnu Rajeevan <Vishnu Rajeevan>
2012-10-14 15:15:42 -0400

    adding notification

--- src/com/dinosaurwithakatana/jobhackcareerbuilder/LocationService.java
+++ src/com/dinosaurwithakatana/jobhackcareerbuilder/LocationService.java
@@ -16,6 +16,8 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 /**
@@ -47,7 +49,36 @@ public class LocationService extends Service {
 	 */
 	public List<Job> getJobs() {
 		try {
-			return queryJobs(mLocation);
+			List<Job> jobsList = queryJobs(mLocation);
+			int jobCount = jobsList.size();
+			
+			NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+			        .setSmallIcon(R.drawable.ic_launcher)
+			        .setContentTitle("My notification")
+			        .setContentText("Hello World!");
+			// Creates an explicit intent for an Activity in your app
+			Intent resultIntent = new Intent(this, MainActivity.class);
+			int mId=0;
+
+			// The stack builder object will contain an artificial back stack for the
+			// started Activity.
+			// This ensures that navigating backward from the Activity leads out of
+			// your application to the Home screen.
+			TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+			// Adds the back stack for the Intent (but not the Intent itself)
+			stackBuilder.addParentStack(LoginActivity.class);
+			// Adds the Intent that starts the Activity to the top of the stack
+			stackBuilder.addNextIntent(resultIntent);
+			PendingIntent resultPendingIntent =
+			        stackBuilder.getPendingIntent(
+			            0,
+			            PendingIntent.FLAG_UPDATE_CURRENT
+			        );
+			mBuilder.setContentIntent(resultPendingIntent);
+			NotificationManager mNotificationManager =
+			    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+			// mId allows you to update the notification later on.
+			mNotificationManager.notify(mId, mBuilder.build());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
